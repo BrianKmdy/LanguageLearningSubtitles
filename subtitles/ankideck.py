c369229de75c5ec3ded1b27b896d73ff3c866127
Brian Moody <brian.kmdy@gmail.com>
2022-10-29 10:59:39 -0700

    Use subtitle parser for anki deck generator

--- subtitles/ankideck.py
+++ subtitles/ankideck.py
@@ -1,5 +1,6 @@
 # Tool to generate a deck of anki cards from a subtitle file
 import chinese_dictionary
+import transcribe
 import genanki
 import argparse
 import json
@@ -12,6 +13,7 @@ import os
 class AnkiDeckGenerator:
     def __init__(self, chinese_dictionary):
         self.chinese_dictionary = chinese_dictionary
+        self.subtitle_parser = transcribe.SubtitleParser()
 
     def _load_template(self, template_file):
         script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -20,20 +22,6 @@ class AnkiDeckGenerator:
 
     def _get_unique_id(self):
         return uuid.uuid4().int >> 64 + 1
-
-    def _translate_subtitles(self, path_in):
-        if self.chinese_dictionary is None:
-            raise Exception('Chinese dictionary not provided')
-        if not os.path.exists(path_in):
-            raise Exception(f'Subtitle file {path_in} does not exist')
-
-        words = set()
-        with open(path_in, 'r', encoding='utf-8') as fin:
-            for line in fin.readlines():
-                if self.chinese_dictionary.is_chinese(line):
-                    words.update(
-                        self.chinese_dictionary.translate(line.rstrip()))
-        return words
 
     def generate_deck(self, subtitle_file, template_file):
         print(f'Generating deck from {subtitle_file}')
@@ -48,12 +36,15 @@ class AnkiDeckGenerator:
             self._get_unique_id(),
             deck_name)
 
-        for hanzi, pinyin, english in self._translate_subtitles(subtitle_file):
-            my_note = genanki.Note(
-                model=model,
-                fields=[pinyin, english])
-            deck.add_note(my_note)
-
+        # Generate the translations for the cards
+        for _, _, text in self.subtitle_parser.parse_subtitles(subtitle_file):
+            for line in text:
+                for _, pinyin, english in self.chinese_dictionary.translate(line):
+                    my_note = genanki.Note(
+                        model=model,
+                        fields=[pinyin, english])
+                    deck.add_note(my_note)
+            
         genanki.Package(deck).write_to_file(f'{subtitle_file.split(".")[0]}.apkg')
 
 
